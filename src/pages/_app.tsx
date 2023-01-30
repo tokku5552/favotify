6f26be4d34afdea7ec6c56dbd0a574b2735c50aa
tokku5552 <physics.0121@gmail.com>
2023-01-30 23:11:38 +0900

    add: dev用にnoindexを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        {/* 本当はdevelopmentのみ */}
+        <meta name='robots' content='noindex' />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
