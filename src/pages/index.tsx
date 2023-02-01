de7f604c2ef2f42f838ef5fc0de9334c5d0c8853
klsh1tt <klsh1tt@gmail.com>
2023-02-01 21:15:38 +0900

    add アプリ名、機能説明、Spotifyアカウントリンクボタン

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ import { Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import NavigationBar from '../components/NavigationBar';
+import {
+  Box,
+  Text,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
@@ -15,11 +21,45 @@ const Home: NextPage = () => {
             <Link href='/setting'>Setting Page</Link>
           </li>
         </ul>
-        <center>
-          <Heading>favotify</Heading>
-        </center>
       </NavigationBar>
-    </>
+        <Stack
+          background={"#1ED760"}
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            >
+            <Text as={'span'} color={'#FFFFFF'}>
+              favotify
+            </Text>
+          </Heading>
+          <Text color={'#FFFFFF'}>
+          機能の説明とSpotifyへのアカウントリンクを促す文章。
+          <br />
+          機能の説明とSpotifyへのアカウントリンクを促す文章。
+          <br />
+          機能の説明とSpotifyへのアカウントリンクを促す文章。
+          <br />
+          機能の説明とSpotifyへのアカウントリンクを促す文章。
+          <br />
+          機能の説明とSpotifyへのアカウントリンクを促す文章。
+          <br />
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Button>
+              Spotifyにアクセス
+            </Button>
+          </Stack>
+        </Stack>
+        </>
   );
 };
 
