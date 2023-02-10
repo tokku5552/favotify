bab68ea035bd42aa9f7e9276d58a950066d12133
klsh1tt <klsh1tt@gmail.com>
2023-02-10 16:18:05 +0900

    delete: animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,21 @@
-import { Box, Button, Flex, Heading, keyframes, Link, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import type { NextPage } from 'next';
-
-const animationKeyframes = keyframes`
-  0% { transform: scale(1); border-radius: 20%; }
-  
-  100% { transform: scale(2) ; border-radius: 20%; }
-`;
-
-const animation = `${animationKeyframes} 2s ease-in-out infinite`;
 
 const Home: NextPage = () => {
   return (
     <>
       <Flex
         as={motion.div}
-        animation={animation}
         minH={'100vh'}
         align={'center'}
         justify={'center'}
         backgroundImage="repeating-radial-gradient(circle at center, #1ed760, #1db954 50%)"
       >
         <Stack
-
           as={Box}
           textAlign={'center'}
-          spacing={{ base: 8, md: 14 }}
+          spacing={{ base: 8, md: 10 }}
           py={{ base: 20, md: 36 }}>
           <Heading
             fontWeight={600}
@@ -59,15 +49,11 @@ const Home: NextPage = () => {
                 Spotifyにアクセス
               </Button>
             </Link>
-
           </Stack>
         </Stack>
       </Flex>
-
     </>
   );
 };
 
 export default Home;
-
-
