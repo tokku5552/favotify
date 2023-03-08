32f7df82182f71f190ac9555b42644b09f3d8f43
klsh1tt <klsh1tt@gmail.com>
2023-03-08 13:19:03 +0900

    wip Vstack削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, HStack, Link, Stack, Text, VStack ,Box} from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Link, Stack, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
@@ -6,65 +6,69 @@ const Home: NextPage = () => {
     <>
       <Flex
         minH={'100vh'}
+        align="center"
         justify={'center'}
         backgroundColor={'black'}
       >
-        <VStack>
-          <HStack
+        <HStack
+          textAlign={'center'}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 10, md: 10 }}
+        >
+          <Text
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
             textAlign={'center'}
-            spacing={{ base: 8, md: 10 }}
-            py={{ base: 10, md: 10 }}
+            as={'span'}
+            color={'#FFFFFF'}
+            width="50%"
           >
-            <Text
-              fontWeight={600}
-              fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-              textAlign={'center'}
-              as={'span'}
-              color={'#FFFFFF'}
+            favotify
+          </Text>
+          <Text
+            width="50%"
+            color={'#FFFFFF'}
+          >
+            機能の説明とSpotifyへのアカウントリンクを促す文章。
+            <br />
+            機能の説明とSpotifyへのアカウントリンクを促す文章。
+            <br />
+            機能の説明とSpotifyへのアカウントリンクを促す文章。
+            <br />
+            機能の説明とSpotifyへのアカウントリンクを促す文章。
+            <br />
+            機能の説明とSpotifyへのアカウントリンクを促す文章。
+            <br />
+            <Link
+              href='/top'
+            >
+              <Button
+                color={'black'}
+                margin="50px 0 0 0"
+              >
+                Spotifyにアクセス
+              </Button>
+            </Link>
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            position={'relative'}
+          >
+          </Stack>
+        </HStack>
 
-            >
-              favotify
-            </Text>
-            <Text
-              color={'#FFFFFF'}
-            >
-              機能の説明とSpotifyへのアカウントリンクを促す文章。
-              <br />
-              機能の説明とSpotifyへのアカウントリンクを促す文章。
-              <br />
-              機能の説明とSpotifyへのアカウントリンクを促す文章。
-              <br />
-              機能の説明とSpotifyへのアカウントリンクを促す文章。
-              <br />
-              機能の説明とSpotifyへのアカウントリンクを促す文章。
-              <br />
-            </Text>
-            <Stack
-              direction={'column'}
-              spacing={3}
-              align={'center'}
-              position={'relative'}
-            >
-            </Stack>
-          </HStack>
-          <Link
-            href='/top'
-          >
-            <Button>
-              Spotifyにアクセス
-            </Button>
-          </Link>
-        </VStack>
       </Flex>
       <Box
-      bg="green.500"
-      height="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Box width="50%" height="100%" bg="black"></Box>
-    </Box>
+        bg="green.500"
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box width="50%" height="100%" bg="black"></Box>
+      </Box>
     </>
   );
 };
