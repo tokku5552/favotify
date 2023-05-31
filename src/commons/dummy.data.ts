951aa1b7f4850da8d0dd663f84a4862f35352d52
klsh1tt <klsh1tt@gmail.com>
2023-05-31 16:27:27 +0900

    dummy data追加

--- src/commons/dummy.data.ts
+++ src/commons/dummy.data.ts
@@ -10,3 +10,36 @@ export const dummyChartData: ChartTableProps = {
     { title: 'Lie Machine', artist: 'Mo Falk, Siks', plays: 12 },
   ],
 };
+
+export const dummyWeeklyChartData: ChartTableProps = {
+  term: 'weekly',
+  charts: [
+    { title: 'weekly', artist: 'ranking', plays: 201 },
+    { title: 'forward', artist: 'ggrks', plays: 36 },
+    { title: 'Final Call - Radio Edit', artist: 'フロリアン・ピカソ', plays: 30 },
+    { title: 'Touch & Go', artist: 'Bingo Players, Oomloud', plays: 21 },
+    { title: 'Lie Machine', artist: 'Mo Falk, Siks', plays: 12 },
+  ],
+};
+
+export const dummyMonthlyChartData: ChartTableProps = {
+  term: 'monthly',
+  charts: [
+    { title: 'monthly', artist: 'ranking', plays: 201 },
+    { title: 'forward', artist: 'ggrks', plays: 36 },
+    { title: 'Final Call - Radio Edit', artist: 'フロリアン・ピカソ', plays: 30 },
+    { title: 'Touch & Go', artist: 'Bingo Players, Oomloud', plays: 21 },
+    { title: 'Lie Machine', artist: 'Mo Falk, Siks', plays: 12 },
+  ],
+};
+
+export const dummyYearlyChartData: ChartTableProps = {
+  term: 'yearly',
+  charts: [
+    { title: 'yearly', artist: 'ranking', plays: 201 },
+    { title: 'forward', artist: 'ggrks', plays: 36 },
+    { title: 'Final Call - Radio Edit', artist: 'フロリアン・ピカソ', plays: 30 },
+    { title: 'Touch & Go', artist: 'Bingo Players, Oomloud', plays: 21 },
+    { title: 'Lie Machine', artist: 'Mo Falk, Siks', plays: 12 },
+  ],
+};
